768c10cf9d2037aed738e1a59de429e3df14e432
Jorge Trad <jetradl@gmail.com>
2021-06-09 14:58:19 -0500

    Posterior balance functionality was implemented when confirming a paymente of a service

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -12,8 +12,10 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <form action="{{ route('services.update'), $service }}" method="POST">
-                    <div class="form-group">
+                    
+                    <form action="{{ route('services.update', $service) }}" method="POST">
+                    {{-- <form method="POST"> --}}
+                        <div class="form-group">
                             <label>Concepto</label>
                             <input type="text" name="name" col="2" class="form-control" value="{{ old('name', $service->name) }}" readonly>
                         </div>
@@ -26,7 +28,7 @@
                                     </div>
                                     <div class="col">
                                         <label>Saldo Disponible</label>
-                                        <input type="number" name="balance" value="{{ App\Card::find(Auth::user()->id - 1) -> balance }}" min="0.01" max="5000" step="0.01" class="form-control" readonly>
+                                        <input type="text" name="balance" id="balance" value="${{ App\Card::find(Auth::user()->id - 1) -> balance }}" class="form-control" readonly>
                                     </div>
                                     
                                 </div>
@@ -37,11 +39,11 @@
                                 <div class="row justify-content-center">
                                     <div class="col">
                                         <label>Correo</label>
-                                        <input type="text" name="email" value="{{ Auth::user()->email }}" class="form-control" readonly>
+                                        <input type="email" name="email" value="{{ Auth::user()->email }}" class="form-control" readonly>
                                     </div>
                                     <div class="col">
                                         <label>Saldo Posterior</label>
-                                        <input type="number" name="balance" value="{{ App\Card::find(Auth::user()->id - 1) -> balance }}" min="0.01" max="5000" step="0.01" class="form-control" readonly>
+                                        <input type="text" onload="calcularSaldoPosterior();" name="balance_pos" id="balance_pos" value="${{ App\Card::find(Auth::user()->id - 1) -> balance }}" class="form-control" readonly>
                                     </div>
                                 </div>
                             </div>
@@ -56,11 +58,11 @@
                                     </div>
                                     <div class="col">
                                         <label>Cantidad</label>
-                                        <input id="cantidad" onchange=" calcularTotal();" type="number" name="saldo" value="1" min="1" max="5" step="1" class="form-control" required>
+                                        <input id="cantidad" onchange="changeValues()" type="number" name="saldo" value="0" min="0" max="5" step="1" class="form-control" required>
                                     </div>
                                     <div class="col">
                                         <label>Total</label>
-                                        <input id="total" onload="clacularTotal()" type="text" name="total" value="${{ old('amount', $service->amount) }}" class="form-control" required readonly>
+                                        <input id="total" onload="changeValues()" type="text" name="total" value="$0.00" class="form-control" required readonly>
                                     </div>
                                     
                                 </div>
@@ -85,13 +87,65 @@
         var precio_u = document.getElementById("precio-u").value;
         var cantidad = document.getElementById("cantidad").value;
         
+        var total = precio_u * cantidad;
+
+        return parseFloat(total);
+    }
+
+    function imprimirTotal(){
         const options2 = { style: 'currency', currency: 'USD' };
         const numberFormated = new Intl.NumberFormat('en-US', options2);
 
+        document.getElementById("total").value = numberFormated.format(calcularTotal());
+        
+    }
+
+    function calcularSaldoPosterior(){
+        var balance_pos = document.getElementById("balance_pos").value;
+        var balance = document.getElementById("balance").value;
+
+        const options2 = { style: 'currency', currency: 'USD' };
+        const numberFormated = new Intl.NumberFormat('en-US', options2);
+
+        var precio_u = document.getElementById("precio-u").value;
+        var cantidad = document.getElementById("cantidad").value;
+        
         var total = precio_u * cantidad;
 
-        document.getElementById("total").value = numberFormated.format(total);
+        balance = balance.slice(1);
+
+        balance = parseFloat(balance);
+        
+        if(cantidad <= 5){
+            if(balance > calcularTotal()){
+                console.log("-");
+                console.log(precio_u);
+                sald_pos = balance - calcularTotal();
+                console.log(sald_pos);
+                
+            } /* else {
+                console.log("+");
+                console.log(precio_u);
+                sald_pos = sald_pos + precio_u;
+                console.log(sald_pos);
+            }     */        
+        } 
+        sald_pos = numberFormated.format(sald_pos);
+
+        document.getElementById("balance_pos").value = sald_pos;
+
+        console.log(sald_pos);
+
+        
     }
+
+    function changeValues(){
+        //if(document.getElementById("balance_pos").value.slice(1) > document.getElementById("total").value) {
+            imprimirTotal();
+            calcularSaldoPosterior();
+        
+    }
+
 </script>
 
 @endsection
